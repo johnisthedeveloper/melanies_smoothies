105aab431e9995ab392278f2403c7558993c2a46
JohnDeveloper <johnisthedeveloper@gmail.com>
2024-04-14 01:28:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,11 @@ st.write(
     """
 )
 
-
-
-
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be:', name_on_order)
 
+cnx = st.connection('snowflake')
+session = cnx.session()
 
 # Get the current credentials
 session = get_active_session()
